540c60728d1ff6bec20bc3386c155a30f6c45244
Matteo Bando <bandomatteo@gmail.com>
2025-06-15 14:20:45 +0000

    Add tile module with constants and walkable tile check; update JSDoc configuration to include config.js

--- config.js
+++ config.js
@@ -6,7 +6,21 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//TODO: Not very elegant, FIXME
+/**
+ * @module config
+ * @description
+ * This script loads environment variables from a .env file.
+ * It checks multiple possible paths for the .env file and loads the first one that exists.
+ * If no .env file is found, it logs an error message with the attempted paths.
+ * The loaded configuration is then exported for use in other parts of the application. 
+ * * @property {Object} CONFIG - An object containing the loaded configuration.
+ * @property {string} CONFIG.host - The host value from the .env file.
+ * @property {string} CONFIG.token - The token value from the .env file.
+ * @property {string} CONFIG.tokenSlave - The tokenSlave value from the .env file.
+ * @example
+ * import CONFIG from './config.js';
+ * console.log(CONFIG.host); // Outputs the host value from the .env file
+ */
 const possiblePaths = [
   path.resolve(process.cwd(), '.env'),                  
   path.resolve(process.cwd(), 'src/agent/.env'),        
